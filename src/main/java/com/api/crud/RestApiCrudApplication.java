1161432ff72d3dc9fa98994cd64e4214ab44d9cf
Umar Khan <mdumarkhansb@gmail.com>
2024-10-12 19:18:42 +0530

    Changes done by me

--- src/main/java/com/api/crud/RestApiCrudApplication.java
+++ src/main/java/com/api/crud/RestApiCrudApplication.java
@@ -8,14 +8,6 @@ public class RestApiCrudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiCrudApplication.class, args);
-		System.out.println("Hello India");
-		System.out.println("HFaisal Shahi");
-		System.out.println("HFaisal Shahi");
-		System.out.println("HFaisal Shahi");
-		System.out.println("Hello India");
-		System.out.println("HFaisal Shahi");
-		System.out.println("HFaisal Shahi");
-		System.out.println("HFaisal Shahi");
 
 	}
 
