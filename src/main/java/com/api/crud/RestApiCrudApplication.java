8d5e1d2ef1baeb7ea5d958b0be85d75b01d16861
umarkhansb94 <109720029+umarkhansb94@users.noreply.github.com>
2024-10-12 19:14:15 +0530

    Add files via upload

---
+++ src/main/java/com/api/crud/RestApiCrudApplication.java
@@ -0,0 +1,22 @@
+package com.api.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiCrudApplication.class, args);
+		System.out.println("Hello India");
+		System.out.println("HFaisal Shahi");
+		System.out.println("HFaisal Shahi");
+		System.out.println("HFaisal Shahi");
+		System.out.println("Hello India");
+		System.out.println("HFaisal Shahi");
+		System.out.println("HFaisal Shahi");
+		System.out.println("HFaisal Shahi");
+
+	}
+
+}
